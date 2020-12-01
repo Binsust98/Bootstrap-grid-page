487574be3e167691d4510b1d7556921a594c84fe
binsust98 <binsust98@gmail.com>
2020-12-01 12:56:49 +0530

    bootstrap completed

---
+++ index2.php
@@ -0,0 +1,29 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>assignment2</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam magnam quisquam illo, cumque, soluta fuga blanditiis, autem laudantium dolorum possimus laboriosam tempora commodi et. Illum culpa vitae voluptatibus molestias vel.
+
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae repellendus, vitae accusamus molestiae rerum corporis voluptatum sint, reiciendis voluptatibus atque, eveniet alias pariatur nobis sit quidem quaerat maiores quod. Aspernatur!
+
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae delectus maiores et animi quod aut, nobis autem ea dignissimos ratione libero expedita sint dolor unde laboriosam ut facilis necessitatibus perferendis.
+
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
